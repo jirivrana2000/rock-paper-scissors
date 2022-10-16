b57001bd0765792838254a31a5edd119bd31a312
Jiří Vrána <jirivrana2000@seznam.cz>
2022-10-16 04:34:50 +0200

    connect html+script,add computerchoice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+  const  myArray = [
+    "Rock",
+    "Paper",
+    "Scissors"
+  ];
+let randomItem = myArray[Math.floor(Math.random()*myArray.length)]; 
+}
+
+
+  
+
+
+
+
